afdd80974bfc5baac4d8f709d2e075efd46fc458
Lucas <lucasdahl425@yahoo.com>
2023-01-27 21:00:51 -0800

    - Updated slimesheet

--- engine/animator.js
+++ engine/animator.js
@@ -1,11 +1,64 @@
+// // This is the Animator class
+// class Animator {
+
+//     // This is the constructor for the character
+//     constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, loop) {
+
+//         // Copy all of the paramters into the object.
+//         Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration,});
+
+//         // Set the fields
+//         this.elapsedTime = 0;
+//         this.totalTime = frameCount * frameDuration;
+//     };
+
+//     // This is the draw frame method
+//     drawFrame(tick, ctx, x, y, scale) {
+
+//         // Properties
+//         this.elapsedTime += tick;
+//         const frame = this.currentFrame();
+
+//         // Reset the time so the animation loops
+//         if(this.elapsedTime >= this.totalTime) {
+//             this.elapsedTime -= this.totalTime;
+//         };
+//         // if (this.isDone()) {
+//         //     if (this.loop) {
+//         //         this.elapsedTime -= this.totalTime;
+//         //     } else {
+//         //         return;
+//         //     }
+//         // }
+
+//         ctx.drawImage(this.spritesheet,
+//             this.xStart + (this.width * frame), this.yStart,
+//             this.width, this.height,
+//             x, y,
+//             this.width * scale, this.height * scale);
+
+//     };
+
+//     // This method gets the current frame.
+//     currentFrame() {
+//         return Math.floor(this.elapsedTime / this.frameDuration);
+//     }
+
+//     // This is the isDone method.
+//     isDone() {
+//         return(this.elapsedTime >= this.totalTime);
+//     };
+
+// }
+
 // This is the Animator class
 class Animator {
 
     // This is the constructor for the character
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, loop) {
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
 
         // Copy all of the paramters into the object.
-        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration,});
+        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration});
 
         // Set the fields
         this.elapsedTime = 0;
@@ -20,22 +73,15 @@ class Animator {
         const frame = this.currentFrame();
 
         // Reset the time so the animation loops
-        if(this.elapsedTime >= this.totalTime) {
+        if(this.elapsedTime > this.totalTime) {
             this.elapsedTime -= this.totalTime;
         };
-        // if (this.isDone()) {
-        //     if (this.loop) {
-        //         this.elapsedTime -= this.totalTime;
-        //     } else {
-        //         return;
-        //     }
-        // }
 
         ctx.drawImage(this.spritesheet,
             this.xStart + (this.width * frame), this.yStart,
             this.width, this.height,
             x, y,
-            this.width * scale, this.height * scale);
+            this.width * scale, this.height * scale)
 
     };
 
--- entites/player.js
+++ entites/player.js
@@ -25,22 +25,22 @@ class Player {
     getAnimations() {
 
         // looking right idle
-        this.animations[0] =  new Animator(this.spritesheet, 0, 0, 32, 32, 1, 1000, true);
+        this.animations[0] =  new Animator(this.spritesheet, 0, 0, 32, 32, 1, 1000);
 
         // Moving right 
-        this.animations[1] =  new Animator(this.spritesheet, 0, 0, 32, 32, 2,  0.5, true);
+        this.animations[1] =  new Animator(this.spritesheet, 0, 0, 32, 32, 2,  0.5);
 
         // Left Idle
-        this.animations[2] =  new Animator(this.spritesheet, 64, 0, 32, 32, 1,  1000, true);
+        this.animations[2] =  new Animator(this.spritesheet, 64, 0, 32, 32, 1,  1000);
 
         // Moving left
-        this.animations[3] =  new Animator(this.spritesheet, 64, 0, 32, 32, 2,  0.2, true);
+        this.animations[3] =  new Animator(this.spritesheet, 64, 0, 32, 32, 2,  0.5);
 
         // Back Idle
-        this.animations[4] =  new Animator(this.spritesheet, 128, 0, 32, 32, 1,  1000, true);
+        this.animations[4] =  new Animator(this.spritesheet, 128, 0, 32, 32, 1,  1000);
 
         // Moving back
-        this.animations[5] =  new Animator(this.spritesheet, 128, 0, 32, 32, 2,  0.2, true);
+        this.animations[5] =  new Animator(this.spritesheet, 128, 0, 32, 32, 2,  0.5);
 
     }
 
